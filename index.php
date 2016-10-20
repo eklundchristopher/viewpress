83127524e00f84f7db5e6e44cd47ea32da17c16a
Christopher Eklund <eklundchristopher@gmail.com>
2016-10-20 12:53:31 +0200

    Removed Whoops

--- index.php
+++ index.php
@@ -13,12 +13,10 @@ Domain Path:    /resources/langs
 Text Domain:    viewpress
 */
 
-use Whoops\Run as Whoops;
 use Illuminate\View\Factory;
 use Illuminate\Events\Dispatcher;
 use Illuminate\Container\Container;
 use Illuminate\View\FileViewFinder;
-use Whoops\Handler\PrettyPageHandler;
 use Illuminate\Filesystem\Filesystem;
 use Illuminate\View\Engines\PhpEngine;
 use EklundChristopher\ViewPress\Actions;
@@ -37,18 +35,6 @@ try {
         require_once $composer;
     }
 
-    // Register a Whoops handler, unless one has already been registered.
-    if (class_exists(Whoops::class)) {
-        $whoops = new Whoops;
-        $handlers = array_filter($whoops->getHandlers(), function ($handler) {
-            return $handler instanceof \Whoops\Handler\Handler;
-        });
-
-        if (empty($handlers)) {
-            $whoops->pushHandler(new PrettyPageHandler);
-            $whoops->register();
-        }
-    }
 
     if (! is_admin()) {
         $app = new Application;
